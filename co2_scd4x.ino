8ece062850785a9ae46e82b2febdda52a1d19501
davidkreidler <13180124+davidkreidler@users.noreply.github.com>
2022-01-15 14:18:33 +0100

    Support 4.2" E-Paper Display
    New Layout & functions for 4.2" E-Paper
    4.2" Sleep function enhancements against grey background
    Enable 4.2" via #define EINK_4IN2

--- co2_scd4x.ino
+++ co2_scd4x.ino
@@ -4,6 +4,9 @@
 #include "GUI_Paint.h"
 #include <stdlib.h>
 #include "init.h"
+#define EINK_2IN54V2
+//#define EINK_4IN2
+
 /* welcome */
 #include <EEPROM.h>
 
@@ -52,9 +55,16 @@ RTC_DATA_ATTR int dataReadyErrorCount = 0;
 #endif
 
 void displayWelcome() {
+#ifdef EINK_2IN54V2
   Paint_DrawBitMap(gImage_welcome);
   EPD_1IN54_V2_Display(BlackImage);
   EPD_1IN54_V2_Sleep();
+#endif
+/*#ifdef EINK_4IN2
+  Paint_DrawBitMap(gImage_welcome);
+  EPD_4IN2_Display(BlackImage);
+  EPD_4IN2_Sleep();
+#endif*/
 
   EEPROM.write(0, 1);
   EEPROM.commit();
@@ -69,10 +79,16 @@ void displayWelcome() {
 }
 
 void initOnce() {
-  const int Imagesize = 5000; //25 * 200
-  BlackImage = (UBYTE *)malloc(Imagesize);
+#ifdef EINK_2IN54V2
+  BlackImage = (UBYTE *)malloc(5000);
   Paint_NewImage(BlackImage, EPD_1IN54_V2_WIDTH, EPD_1IN54_V2_HEIGHT, 270, WHITE);
   EPD_1IN54_V2_Init();
+#endif
+#ifdef EINK_4IN2
+  BlackImage = (UBYTE *)malloc(EPD_4IN2_WIDTH / 4 );
+  Paint_NewImage(BlackImage, EPD_4IN2_WIDTH, EPD_4IN2_HEIGHT, 0, WHITE);
+  EPD_4IN2_Init();
+#endif
   Paint_Clear(WHITE); 
   EEPROM.begin(1); //EEPROM_SIZE
   int welcomeDone = EEPROM.read(0);
@@ -84,9 +100,15 @@ void initOnce() {
   scd4x.setTemperatureOffset(tempOffset);
   scd4x.startPeriodicMeasurement();
   
+#ifdef EINK_2IN54V2
   Paint_DrawBitMap(gImage_init);
   EPD_1IN54_V2_Display(BlackImage);
   EPD_1IN54_V2_Sleep();
+#endif
+#ifdef EINK_4IN2
+  EPD_4IN2_Display(BlackImage);
+  EPD_4IN2_Sleep();
+#endif
   //refreshes++;
   delay(3000); // Wait for co2 measurement
   initDone = true;
@@ -126,10 +148,17 @@ void lowBatteryMode() {
   Paint_DrawString_EN(45, 120, "Batterie", &Font20, BLACK, WHITE);
   Paint_DrawString_EN(45, 145, "aufladen", &Font20, BLACK, WHITE);
 
+#ifdef EINK_2IN54V2
   EPD_1IN54_V2_Init();
   EPD_1IN54_V2_Display(BlackImage);
   EPD_1IN54_V2_DisplayPartBaseImage(BlackImage);
   EPD_1IN54_V2_Sleep();
+#endif
+#ifdef EINK_4IN2
+  EPD_4IN2_Init();
+  EPD_4IN2_Display(BlackImage);
+  EPD_4IN2_Sleep();
+#endif
 
   esp_sleep_enable_ext0_wakeup((gpio_num_t)4,1);
   esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_AUTO);   // RTC IO, sensors and ULP co-processor
@@ -246,6 +275,7 @@ void loop(){
     if (BatteryMode && commingFromDeepSleep && new_co2 < co2+10 && new_co2 > co2-10) goto_deep_sleep();
     if (new_co2 > 400) co2 = new_co2;
     
+#ifdef EINK_2IN54V2
     /* co2 */
     if      (co2 > 9999) Paint_DrawNum(27, 78, co2, &bahn_mid, BLACK, WHITE);
     else if (co2 < 1000) Paint_DrawNum(30, 65, co2, &bahn_big, BLACK, WHITE);
@@ -264,6 +294,47 @@ void loop(){
     /* humidity */
     Paint_DrawNum(124, 5, humidity, &bahn_mid, BLACK, WHITE);
     Paint_DrawString_EN(184, 5, "%", &bahn_sml, WHITE, BLACK);   
+#endif
+#ifdef EINK_4IN2
+    /* co2 */
+                                     //Xstart,Ystart
+    if      (co2 > 9999) Paint_DrawNum(102, 88, co2, &bahn_big, BLACK, WHITE);
+    else if (co2 < 1000) Paint_DrawNum(196, 88, co2, &bahn_big, BLACK, WHITE);
+    else                 Paint_DrawNum(149, 88, co2, &bahn_big, BLACK, WHITE);
+    Paint_DrawString_EN(337, 143, "ppmn", &bahn_sml, WHITE, BLACK);
+
+    /* devider lines */
+              //Xstart,Ystart,Xend,Yend
+    Paint_DrawLine( 10, 210, 390, 210, BLACK, DOT_PIXEL_3X3, LINE_STYLE_SOLID);
+    Paint_DrawLine(200, 210, 200, 290, BLACK, DOT_PIXEL_3X3, LINE_STYLE_SOLID);
+
+    /* House synmbol */
+    Paint_DrawLine( 13, 120,  70,  66, BLACK, DOT_PIXEL_3X3, LINE_STYLE_SOLID); //top left
+    Paint_DrawLine( 70,  66, 126, 120, BLACK, DOT_PIXEL_3X3, LINE_STYLE_SOLID); //top right
+    Paint_DrawLine( 27, 109,  27, 171, BLACK, DOT_PIXEL_3X3, LINE_STYLE_SOLID); //left
+    Paint_DrawLine(112, 109, 112, 171, BLACK, DOT_PIXEL_3X3, LINE_STYLE_SOLID); //right
+    Paint_DrawLine( 27, 171, 112, 171, BLACK, DOT_PIXEL_3X3, LINE_STYLE_SOLID); //button
+    Paint_DrawLine( 99,  70,  99,  91, BLACK, DOT_PIXEL_5X5, LINE_STYLE_SOLID); //chimney
+                      //X_Center,Y_Center,radius
+    Paint_DrawCircle(    54, 132,  16, BLACK, DOT_PIXEL_3X3, DRAW_FILL_EMPTY);      //C
+    Paint_DrawRectangle( 58, 112,  72, 152, WHITE, DOT_PIXEL_1X1, DRAW_FILL_FULL);  //C
+    Paint_DrawCircle(    80, 132,  16, BLACK, DOT_PIXEL_3X3, DRAW_FILL_EMPTY);      //O
+    Paint_DrawNum(       94, 145,   2, &bahn_sml, BLACK, WHITE);                    //2
+    
+    /* temperature */
+    if (temperature < 10.0f) Paint_DrawNum(69, 220, temperature, &bahn_big, BLACK, WHITE);
+    else                     Paint_DrawNum(32, 220, temperature, &bahn_big, BLACK, WHITE);
+    Paint_DrawString_EN(140, 220, "*C", &bahn_sml, WHITE, BLACK);
+    Paint_DrawLine(137, 287, 137, 287, BLACK, DOT_PIXEL_4X4, LINE_STYLE_SOLID);
+    
+    char decimal[1];
+    sprintf(decimal, "%d", ((int)(temperature*10))%10);
+    Paint_DrawString_EN(145, 247, decimal, &bahn_mid, WHITE, BLACK);
+  
+    /* humidity */
+    Paint_DrawNum(240, 220, humidity, &bahn_big, BLACK, WHITE);
+    Paint_DrawString_EN(340, 220, "%", &bahn_sml, WHITE, BLACK);
+#endif
   }
 
   if (!BatteryMode) {
@@ -310,22 +381,38 @@ void loop(){
     Paint_DrawString_EN(100, 180, batteryvolt, &Font20, WHITE, BLACK);*/
         
     uint8_t percentage = 100;
+#ifdef EINK_2IN54V2
     voltage += 0.11; // offset for type of Battery used
+#endif
     if (voltage <= 3.62) 
       percentage = 75 * pow((voltage-3.2),2);
     else if (voltage <= 4.19)
       percentage = 2836.9625 * pow(voltage,4) - 43987.4889 * pow(voltage,3) + 255233.8134 * pow(voltage,2) - 656689.7123*voltage + 632041.7303; 
     
+#ifdef EINK_2IN54V2
     //                  Xstart,Ystart,Xend,Yend
     Paint_DrawRectangle( 15, 145, 120, 169, BLACK, DOT_PIXEL_2X2, DRAW_FILL_EMPTY);
     Paint_DrawRectangle(120, 149, 125, 165, BLACK, DOT_PIXEL_2X2, DRAW_FILL_EMPTY);
     Paint_DrawRectangle(15, 145, 105*(percentage/100.0)+15, 169, BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);
+#endif
+#ifdef EINK_4IN2
+    Paint_DrawRectangle(225, 10, 330, 34, BLACK, DOT_PIXEL_2X2, DRAW_FILL_EMPTY);
+    Paint_DrawRectangle(330, 14, 335, 30, BLACK, DOT_PIXEL_2X2, DRAW_FILL_EMPTY); //nippel
+    Paint_DrawRectangle(225, 10, 105*(percentage/100.0)+225, 34, BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);
+
+    char batterpercent[8] = "";
+    dtostrf(percentage, 3, 0, batterpercent);
+    char percent[10] = "%";
+    strcat(batterpercent, percent);
+    Paint_DrawString_EN(342, 10, batterpercent, &bahn_sml, WHITE, BLACK);
+#endif
   }
 
   /*Paint_DrawNum(0, 0, (int32_t)refreshes, &Font20, BLACK, WHITE);
   char dataReadyErrorCount_c[10];
   sprintf(dataReadyErrorCount_c, "%d", dataReadyErrorCount);
   Paint_DrawString_EN(160, 180, dataReadyErrorCount_c, &Font20, WHITE, BLACK);*/
+#ifdef EINK_2IN54V2
   if (refreshes == 1) {
     EPD_1IN54_V2_Init();
     EPD_1IN54_V2_Display(BlackImage);
@@ -334,6 +421,16 @@ void loop(){
     EPD_1IN54_V2_DisplayPart(BlackImage); // partial update
   }
   EPD_1IN54_V2_Sleep();
+#endif
+#ifdef EINK_4IN2
+  EPD_4IN2_Init();
+  if (refreshes == 1) {
+    EPD_4IN2_Display(BlackImage);
+  } else {
+    EPD_4IN2_PartialDisplay(0, 0, EPD_4IN2_WIDTH, EPD_4IN2_HEIGHT, BlackImage);
+  }
+  EPD_4IN2_Sleep();
+#endif
   if (refreshes == 720) { // every hour or every six hours on battery
     refreshes = 0; // force full update
   }
